5710ab8587c3c7f8c1df4b55a2611428ed041719
Joachim Vanneste <2460800V@student.gla.ac.uk>
2022-10-06 16:53:14 +0100

    crawer can now crawl different pages

--- src/crawlVideos.py
+++ src/crawlVideos.py
@@ -1,26 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-for pagenumber in range(1,2):
-	url = "https://archive.org/details/classic_tv_commercials?query=tv+commercials&and%5B%5D=mediatype%3A%22movies%22&sort=&page="+pagenumber
+url = "https://archive.org/details/classic_tv_commercials?query=tv+commercials&and%5B%5D=mediatype%3A%22movies%22&sort=&page="
 
-c=0
-r = requests.get(url)
- 
-
-soup = BeautifulSoup(r.content, 'html5lib')
 
 class_list = []
+count = 0
 
-divs = soup.find('div', class_ = "results")
+for pagenumber in range(1,4):
+	print("Crawling page " + str(pagenumber) + "...")
+	url = url + str(pagenumber)
+	r = requests.get(url)
+	soup = BeautifulSoup(r.content, 'html5lib')
+	divs = soup.find('div', class_ = "results")
 
+	for divtag in divs.find_all('div', class_="item-ia"):
+		count +=1
 
-for divtag in divs.find_all('div', class_="item-ia"):
-	c+=1
-	print(divtag["data-id"])
-
-print(c)
-
-
-
+print(count)
 #tags = {tag.name for tag in soup.find_all()}
