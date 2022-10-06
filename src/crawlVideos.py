794984dd51dd4400e8b6a6700bb2d6533d41cb25
Joachim Vanneste <2460800V@student.gla.ac.uk>
2022-10-06 16:47:41 +0100

    crawler added

--- src/crawlVideos.py
+++ src/crawlVideos.py
@@ -1,16 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
+for pagenumber in range(1,2):
+	url = "https://archive.org/details/classic_tv_commercials?query=tv+commercials&and%5B%5D=mediatype%3A%22movies%22&sort=&page="+pagenumber
 
-url = "https://archive.org/details/classic_tv_commercials?query=tv+commercials&sort=&and[]=mediatype%3A%22movies%22"
- 
-
+c=0
 r = requests.get(url)
  
 
 soup = BeautifulSoup(r.content, 'html5lib')
 
+class_list = []
 
-video_tags = soup.find(class_="item-ia hov")
+divs = soup.find('div', class_ = "results")
 
-print(video_tags)
+
+for divtag in divs.find_all('div', class_="item-ia"):
+	c+=1
+	print(divtag["data-id"])
+
+print(c)
+
+
+
+#tags = {tag.name for tag in soup.find_all()}
