f0e434c8f3ab9b0fc03ca4b666c03d8b3831b018
Joachim Vanneste <2460800V@student.gla.ac.uk>
2022-10-10 14:28:48 +0100

    crawler changes

--- src/crawlVideos.py
+++ src/crawlVideos.py
@@ -2,32 +2,45 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+def main(a,b):
+	#used curl initially 
+	url = "https://archive.org/details/classic_tv_commercials?query=tv+commercials&and%5B%5D=mediatype%3A%22movies%22&sort=&page="
 
-url = "https://archive.org/details/classic_tv_commercials?query=tv+commercials&and%5B%5D=mediatype%3A%22movies%22&sort=&page="
+	#no duplicates
+	video_links = set()
+	count = 0
 
-#no duplicates
-video_links = set()
-count = 0
+	for pagenumber in range(a,b):
+		print("Crawling page " + str(pagenumber) + "...")
+		url = url + str(pagenumber)
+		try:
+			r = requests.get(url, timeout = 10)
+			soup = BeautifulSoup(r.content, 'html.parser')
+			
+		except requests.RequestException as e:
+			print("FUCKKK!!!")
 
-for pagenumber in range(1,100):
-	print("Crawling page " + str(pagenumber) + "...")
-	url = url + str(pagenumber)
-	r = requests.get(url)
-	soup = BeautifulSoup(r.content, 'html5lib')
-	divs = soup.find('div', class_ = "results")
+		divs = soup.find('div', class_ = "results")
+		print(len(divs))
+		for itemai in divs.find_all('div', class_="item-ia"):
+			for C234 in itemai.find_all('div', class_="C234"):
+				for item in C234.find_all('div', class_="item-ttl"):
+					for atag in item.find_all('a'):
+						video_links.add(atag["href"])
+						count +=1
+		print(count)
 
-	for itemai in divs.find_all('div', class_="item-ia"):
-		for C234 in itemai.find_all('div', class_="C234"):
-			for item in C234.find_all('div', class_="item-ttl"):
-				for atag in item.find_all('a'):
-					video_links.add(atag["href"])
-					count +=1
-					print(count)
+	print("Total videos: " + str(count))
 
-print("Total videos: " + str(count))
+	print("Writing links to csv...")
+	with open('videolinks.csv','a') as f:
+	    for line in video_links:
+	        f.write(line)
+	        f.write('\n')
 
-print("Writing links to csv...")
-with open('videolinks.csv','w') as f:
-    for line in video_links:
-        f.write(line)
-        f.write('\n')
+
+
+if __name__ == '__main__':
+	for i in range(1,9,2):
+		main(i,i+2)
+
