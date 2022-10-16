8dcccd94f47b099f967f345da5a0fb7bafe89cd8
Joachim Vanneste <2460800V@student.gla.ac.uk>
2022-10-16 16:05:09 +0100

    bug fixes

--- src/annotateVideos.py
+++ src/annotateVideos.py
@@ -2,19 +2,29 @@ import csv
 import os
 import random
 import subprocess, sys
+import pandas as pd
 
 videos=[]
+annotations=[]
+
+NUM_OF_VIDEOS = 1
+
+
 for filename in os.listdir("../data/videos"):
    with open(os.path.join("../data/videos", filename), 'r') as f:
        videos.append(filename)
 
-
 random.shuffle(videos)
-print(videos[0])
 
+for i in range(NUM_OF_VIDEOS):
+	video_id = videos[i]
+	video = '../data/videos/'+str(video_id)
 
-f = '../data/videos/'+str(videos[0])
-
-
-opener = "open" if sys.platform == "darwin" else "xdg-open"
-subprocess.call([opener, f])
+	opener = "open" if sys.platform == "darwin" else "xdg-open"
+	subprocess.call([opener, video])
+	exciting = input("Exciting (0/1): ")
+	funny = input("Funny (0/1): ")
+	annotations.append([str(video_id[:len(video_id)-4]), exciting, funny])
+	
+df = pd.DataFrame(annotations, columns = ['id', 'Exciting', 'Funny'])
+print(df)
