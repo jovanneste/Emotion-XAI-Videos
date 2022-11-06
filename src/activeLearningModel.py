598703f70b428a6f01bc9b227fc775cca99da773
Joachim Vanneste <2460800V@student.gla.ac.uk>
2022-11-06 17:07:55 +0000

    Use annotate function from annotateVideo

--- src/activeLearningModel.py
+++ src/activeLearningModel.py
@@ -1,18 +1,18 @@
 from predict import *
 from  annotateVideos import annotate
 import os.path
+import subprocess, sys
 
 
 def sortDictionary(d):
-    return {k: v for k, v in sorted(d.items(), key=lambda item: item[1])}
+    return sorted(d.items(), key=lambda x: abs(x[1] - 0.5))
 
-def annotate_batch(batch, annotate_num):
-    print('aaaa')
+def annotate_batch(batch, annotate_num, videos):
     j=0
     annotations = {}
     for key in batch:
         if j<annotate_num:
-            annotations.update({key:annotate(key)})
+            annotations.update({key:annotate(key, videos)})
             j+=1
     return annotations
 
@@ -20,6 +20,7 @@ def annotate_batch(batch, annotate_num):
 def uncertaintySampling(n, annotate_num):
     funny_batch = {}
     exciting_batch = {}
+    videos = []
     for i in range(1, 10):
         print(i)
         video_path = "../data/videos/"+str(i)+".mp4"
@@ -30,17 +31,21 @@ def uncertaintySampling(n, annotate_num):
             continue
         if (0.5-n) < result[0] < (0.5+n):
             exciting_batch.update({i:float(result[0])})
-        if (0.5-n) < result[1] < (0.5+n):
+        elif (0.5-n) < result[1] < (0.5+n):
+            # if video already in list it will probably be annotated anyway
             funny_batch.update({i:float(result[1])})
-
 
     funny_batch = sortDictionary(funny_batch)
     exciting_batch = sortDictionary(exciting_batch)
 
+    for filename in os.listdir("../data/videos"):
+       with open(os.path.join("../data/videos", filename), 'r') as f:
+           videos.append(filename)
+
     print("Batch to annotate (funny)...", funny_batch)
-    #funny_labels = annotate_batch(funny_batch, annotate_num)
+    #funny_labels = annotate_batch(funny_batch, annotate_num, videos)
     print("Batch to annotate (exciting)...", exciting_batch)
-    #funny_labels = annotate_batch(exciting_batch, annotate_num)
+    #funny_labels = annotate_batch(exciting_batch, annotate_num, videos)
 
 
 
--- src/annotateVideos.py
+++ src/annotateVideos.py
@@ -4,32 +4,36 @@ import random
 import subprocess, sys
 import pandas as pd
 
-videos=[]
-annotations=[]
-print('what')
-NUM_OF_VIDEOS = 100
-
-def annotate(i):
-    print('the')
+def annotate(i, videos):
     video_id = videos[i]
     video = '../data/videos/'+str(video_id)
     opener = "open" if sys.platform == "darwin" else "xdg-open"
     subprocess.call([opener, video])
-    print("Video: ", i)
+    print("Video:", i)
     exciting = input("Exciting (0/1): ")
     funny = input("Funny (0/1): ")
     return [str(video_id[:len(video_id)-4]), exciting, funny]
 
+def main():
+    videos=[]
+    annotations=[]
+    NUM_OF_VIDEOS = 100
 
-for filename in os.listdir("../data/videos"):
-   with open(os.path.join("../data/videos", filename), 'r') as f:
-       videos.append(filename)
+    for filename in os.listdir("../data/videos"):
+       with open(os.path.join("../data/videos", filename), 'r') as f:
+           videos.append(filename)
 
-random.shuffle(videos)
+    random.shuffle(videos)
 
-for i in range(NUM_OF_VIDEOS):
-	annotations.append(annotate(i))
+    for i in range(1, NUM_OF_VIDEOS):
+        try:
+            annotations.append(annotate(i, videos))
+        except:
+            print("No video:", i)
 
-df = pd.DataFrame(annotations, columns = ['id', 'Exciting', 'Funny'])
-df.to_csv("../data/annotatedVideos.csv", sep='\t')
-print(df.head())
+    df = pd.DataFrame(annotations, columns = ['id', 'Exciting', 'Funny'])
+    df.to_csv("../data/annotatedVideos.csv", sep='\t')
+    print(df.head())
+
+if __name__ == '__main__':
+    main()
