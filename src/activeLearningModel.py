a020f5c4f870dd7e37c00e2be0f29f3895f15922
Joachim Vanneste <2460800V@student.gla.ac.uk>
2022-11-06 14:40:49 +0000

    Annotate uncertainty sampling

--- src/activeLearningModel.py
+++ src/activeLearningModel.py
@@ -1,10 +1,25 @@
 from predict import *
+from  annotateVideos import annotate
 import os.path
 
-def uncertaintySampling(range):
-    funny_batch = []
-    exciting_batch = []
-    for i in range(1, 100):
+
+def sortDictionary(d):
+    return {k: v for k, v in sorted(d.items(), key=lambda item: item[1])}
+
+def annotate_batch(batch, annotate_num):
+    j=0
+    annotations = []
+    for key in batch:
+        if j<annotate_num:
+            annotations.append(annotate(key))
+            j+=1
+    return annotations
+
+
+def uncertaintySampling(n, annotate_num):
+    funny_batch = {}
+    exciting_batch = {}
+    for i in range(1, 10):
         print(i)
         video_path = "../data/videos/"+str(i)+".mp4"
         if os.path.exists(video_path):
@@ -12,13 +27,18 @@ def uncertaintySampling(range):
             result = predict(data)
         else:
             continue
-
-        if 0.45 < result[0] < 0.55:
-            exciting_batch.append({i:result[0]})
-        if 0.45 < result[1] < 0.55:
-            funny_batch.append({i:result[1]})
-    print(exciting_batch)
-    print(funny_batch)
+        if (0.5-n) < result[0] < (0.5+n):
+            exciting_batch.update({i:float(result[0])})
+        if (0.5-n) < result[1] < (0.5+n):
+            funny_batch.update({i:float(result[1])})
 
 
-uncertaintySampling()
+    funny_batch = sortDictionary(funny_batch)
+    exciting_batch = sortDictionary(exciting_batch)
+    print("Batch to annotate (funny)...", funny_batch)
+    funny_labels = annotate_batch(funny_batch, annotate_num)
+
+
+
+
+uncertaintySampling(0.1, 5)
--- src/annotateVideos.py
+++ src/annotateVideos.py
@@ -9,6 +9,16 @@ annotations=[]
 
 NUM_OF_VIDEOS = 100
 
+def annotate(i):
+    video_id = videos[i]
+    video = '../data/videos/'+str(video_id)
+    opener = "open" if sys.platform == "darwin" else "xdg-open"
+    subprocess.call([opener, video])
+    print("Video: ", i)
+    exciting = input("Exciting (0/1): ")
+    funny = input("Funny (0/1): ")
+    return [str(video_id[:len(video_id)-4]), exciting, funny]
+
 
 for filename in os.listdir("../data/videos"):
    with open(os.path.join("../data/videos", filename), 'r') as f:
@@ -17,16 +27,8 @@ for filename in os.listdir("../data/videos"):
 random.shuffle(videos)
 
 for i in range(NUM_OF_VIDEOS):
-	video_id = videos[i]
-	video = '../data/videos/'+str(video_id)
+	annotations.append(annotate(i))
 
-	opener = "open" if sys.platform == "darwin" else "xdg-open"
-	subprocess.call([opener, video])
-	print("Video: ", i)
-	exciting = input("Exciting (0/1): ")
-	funny = input("Funny (0/1): ")
-	annotations.append([str(video_id[:len(video_id)-4]), exciting, funny])
-	
 df = pd.DataFrame(annotations, columns = ['id', 'Exciting', 'Funny'])
 df.to_csv("../data/annotatedVideos.csv", sep='\t')
 print(df.head())
