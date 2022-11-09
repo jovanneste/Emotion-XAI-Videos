3b060f240f238863f4f80d7d238d860428743250
Joachim Vanneste <2460800V@student.gla.ac.uk>
2022-11-09 19:08:41 +0000

    Move annotated videos to new directory

---
+++ src/splitVideos.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+
+
+df = pd.read_csv('../data/annotatedVideos.csv', delim_whitespace=True)
+
+for index, row in df.iterrows():
+	try:
+		idn = row['id']
+		video = '../data/videos/' + str(idn) + '.mp4'
+		new_path = '../data/videos/test_videos/' + str(idn) + '.mp4'
+		os.rename(video, new_path)
+		print('Moved:', video)
+	except:
+		print('Video does not exist')
