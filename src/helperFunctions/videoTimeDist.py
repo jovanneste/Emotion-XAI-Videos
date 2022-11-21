fb796d0c7fc4447f9b003518818b38878330ff95
Joachim Vanneste <2460800V@student.gla.ac.uk>
2022-11-21 10:59:14 +0000

    delete videos over 60seconds

--- src/helperFunctions/videoTimeDist.py
+++ src/helperFunctions/videoTimeDist.py
@@ -4,20 +4,30 @@ import os
 import matplotlib.pyplot as plt
 
 times = []
-i=0
+kept = []
+i = 0
+
+delete = True
 for filename in os.listdir("../../data/videos/train_videos/"):
-   with open(os.path.join("../../data/videos/train_videos/", filename), 'r') as f:
-       try:
-           data = cv2.VideoCapture("../../data/videos/train_videos/" + filename)
-           frames = data.get(cv2.CAP_PROP_FRAME_COUNT)
-           fps = data.get(cv2.CAP_PROP_FPS)
-           seconds = round(frames/fps)
-           if seconds > 300:
-               i+=1
-           else:
-               times.append(seconds)
-       except:
-           print("Video failed")
+    with open(os.path.join("../../data/videos/train_videos/", filename), 'r') as f:
+        try:
+            data = cv2.VideoCapture(
+                "../../data/videos/train_videos/" + filename)
+            frames = data.get(cv2.CAP_PROP_FRAME_COUNT)
+            fps = data.get(cv2.CAP_PROP_FPS)
+            seconds = round(frames/fps)
+            if seconds > 300:
+                i += 1
+            else:
+                times.append(seconds)
+            if (delete):
+                if seconds > 60:
+                    print("Delete")
+                    os.remove("../../data/videos/train_videos/"+str(filename))
+                else:
+                    kept.append(filename)
+        except:
+            print("Video failed")
 
 
 fig, ax = plt.subplots(1, 1)
@@ -28,3 +38,6 @@ ax.set_xlabel('Length of video (seconds)')
 
 plt.savefig('videoTimeDistribution.png')
 plt.show()
+
+
+print("Kept", len(kept))
