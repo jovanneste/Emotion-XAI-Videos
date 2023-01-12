2b7f8432b57c59493956082ff09f02ed9052f1e3
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-01-12 21:35:00 +0000

    trying to get LIME to work

--- src/explainability/explainFrame.py
+++ src/explainability/explainFrame.py
@@ -8,35 +8,31 @@ import cv2
 from tensorflow import keras
 import keras.utils as image
 import numpy as np
-# from keras.applications import inception_v3 as inc_net
-#
-# global model
-# model = keras.models.load_model('../../data/models/predict_model')
+
+global model
+model = keras.models.load_model('../../data/models/predict_model')
+
+print(model.summary())
 
 def transform_img_fn(img_path):
     print('transform_img_fn')
-    out = []
-    img = image.load_img(img_path, target_size=(299, 299))
+    img = image.load_img(img_path)
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
-    x = inc_net.preprocess_input(x)
-    out.append(x)
-    return np.vstack(out)
+    return np.squeeze(x)
 
 explainer = lime_image.LimeImageExplainer()
-#frame = transform_img_fn('../../data/frames/frame300.jpg')
-print("Video data")
-print(load_sample('../../data/videos/train_videos/1.mp4').shape)
-
 print("Frame data")
-data = load_sample('../../data/frames/frame300.jpg')
+frame = transform_img_fn('../../data/frames/frame300.jpg')
+print(frame.shape)
+
+print("Video data")
+data = load_sample('../../data/videos/train_videos/43.mp4')
 print(data.shape)
 
 
-sys.exit()
-
 # create auxilary local model
-print('creating explaination')
-explanation  = explainer.explain_instance(frame.astype('double'), model.predict, top_labels=5, hide_color=0, num_samples=1000)
+print('Creating explaination')
+explanation  = explainer.explain_instance(data.astype('double'), model.predict, top_labels=2, hide_color=None, num_samples=1000)
 
 print(explanation)
