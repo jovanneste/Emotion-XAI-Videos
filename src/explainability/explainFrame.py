c2f850022475fe6495764d2666098f2150df324d
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-01-17 10:27:03 +0000

    superpixel segmentation added

--- src/explainability/explainFrame.py
+++ src/explainability/explainFrame.py
@@ -1,22 +1,37 @@
+import matplotlib.pyplot as plt
+from skimage import io
+from skimage.util import img_as_float
+from skimage.segmentation import mark_boundaries
+from skimage.segmentation import slic
+import numpy as np
+import keras.utils as image
+from tensorflow import keras
+import cv2
+from evaluateModel import *
 import lime
 from lime import lime_image
 import os
 import sys
 sys.path.append('../')
-from evaluateModel import *
-import cv2
-from tensorflow import keras
-import keras.utils as image
-import numpy as np
-
-global model
-model = keras.models.load_model('../../data/models/predict_model')
-
-#print(model.summary())
 
 
+# global model
+# model = keras.models.load_model('../../data/models/predict_model')
 
+# print(model.summary())
 
+image = img_as_float(io.imread('../../data/frames/frame4.jpg'))
+
+for numSegments in (200):
+	segments = slic(image, n_segments=numSegments, sigma=5, start_label=1)
+	fig = plt.figure("Superpixels -- %d segments" % (numSegments))
+	ax = fig.add_subplot(1, 1, 1)
+	print("Number:", numSegments)
+	print("Segments:", segments)
+	ax.imshow(mark_boundaries(image, segments))
+	plt.axis("off")
+
+plt.show()
 
 
 
