95176c9fceaf757b92c222ce1282bbcc06cfcbf1
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-01-17 17:35:27 +0000

    super pixel segmentation and visualisation finished

--- src/explainability/explainFrame.py
+++ src/explainability/explainFrame.py
@@ -9,7 +9,7 @@ from tensorflow import keras
 import keras.utils as image
 import numpy as np
 import random
-
+import copy
 from skimage.segmentation import slic
 from skimage.segmentation import mark_boundaries
 from skimage.util import img_as_float
@@ -22,26 +22,55 @@ import matplotlib.pyplot as plt
 
 #print(model.summary())
 
-def getSuperPixels(image_path, n=300):
+
+def visualiseSuperPixels(segments, image):
+    for (i, segVal) in enumerate(np.unique(segments)):
+        print("[x] inspecting segment %d" % (i))
+        mask = np.zeros(image.shape[:2], dtype = "uint8")
+        mask[segments == segVal] = 255
+        cv2.imshow("Mask", mask)
+        cv2.imshow("Applied", cv2.bitwise_and(image, image, mask = mask))
+        key = cv2.waitKey(0)
+
+
+
+def createNeighbourhoodSet(image_path, blocks, perturbed_num, pixel_segments=300, visualise=False):
     image = img_as_float(io.imread(image_path))
-    segments = slic(image, n_segments=n, sigma=5, start_label=1)
-    fig = plt.figure("Superpixels -- %d segments" % (n))
+    segments = slic(image, n_segments=pixel_segments, sigma=5, start_label=1)
+
+    # visualise super pixel regions
+    fig = plt.figure("Superpixels -- %d segments" % (pixel_segments))
     ax = fig.add_subplot(1, 1, 1)
     ax.imshow(mark_boundaries(image, segments))
     plt.axis("off")
     plt.show()
-    return segments
 
+    if visualise: visualiseSuperPixels(segments, image)
 
-def createNeighbourhoodSet(image_path, blocks, perturbed_num):
-    segments = getSuperPixels(image_path)
     for i in range(perturbed_num):
+        frame = cv2.imread(image_path)
+        indexes = []
         nums = random.sample(range(1,np.max(segments)), blocks)
-        print(nums)
+        for n in nums:
+            pos = np.transpose(np.where(segments==n))
+            for p in pos:
+                indexes.append(p)
+        indexes = np.asarray(indexes)
+
+        print("Creating image", i+1)
+        print("Masking out "+str(indexes.shape[0])+" pixels")
+        for index in indexes:
+            frame[index[0], index[1]] = (0,0,0)
+        cv2.imwrite("test" + str(i+1) + ".jpg", frame)
 
 
-path = '../../data/frames/frame4.jpg'
-createNeighbourhoodSet(path, 20, 1)
+
+
+
+
+
+path = '../../data/frames/frame429.jpg'
+createNeighbourhoodSet(path, 10, 5)
 
 # explainer = lime_image.LimeImageExplainer()
 #
