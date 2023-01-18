7d59bd33e34a9bb7a6cf816c423b9577b26d21a1
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-01-18 19:03:32 +0000

    remove code

--- src/explainability/explainPrediction.py
+++ src/explainability/explainPrediction.py
@@ -10,86 +10,6 @@ import glob
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-def plot(x, y, ranges):
-    lowers, uppers = [], []
-    for r in ranges:
-        for point in x:
-            if r[0]==point:
-                lowers.append(np.abs(r[0] - r[1]))
-                uppers.append(np.abs(r[0] - r[2]))
-    fig, ax = plt.subplots()
-    ax.errorbar(x, y, xerr=[lowers, uppers], fmt='-o')
-    ax.set_xlabel("Frames")
-    ax.set_ylabel("Frame importance")
-    plt.show()
-
-def getSortedFrames():
-    frames = []
-    for filename in glob.glob('../../data/frames/*.jpg'):
-        filename = filename.split('/')[4].split('.')[0]
-        frames.append(int(filename[5:]))
-    return sorted(frames)
-
-def sortDict(d, key):
-    if key:
-        return {k: v for k, v in sorted(d.items(), key=lambda item: item[0])}
-    else:
-        return {k: v for k, v in sorted(d.items(), key=lambda item: item[1])}
-
-def maskFrames(video_path, n):
-    print("Quantising video...")
-    ranges, fps, frameSize = getFrames(video_path, n)
-    data = load_sample(video_path)
-    result = predict(data, model)
-    exciting_label = result[0]
-    print("Original video result", result)
-    print("Exciting label =", exciting_label)
-    print(ranges)
-
-    differences = {}
-    frames = getSortedFrames()
-
-    for r in ranges:
-        keyFrame = r[0]
-        print("Building new video without", keyFrame)
-        lowerBound = r[1]
-        upperBound = r[2]
-        out = cv2.VideoWriter(str(keyFrame) + '.mp4', cv2.VideoWriter_fourcc('m','p','4','v'), fps, frameSize)
-        for frame in frames:
-            if lowerBound <= frame <= upperBound:
-                continue
-            else:
-                img = cv2.imread('../../data/frames/frame' +str(frame)+ '.jpg')
-                out.write(img)
-
-        out.release()
-
-        data = load_sample(str(keyFrame) + '.mp4')
-        result = predict(data, model)
-        print(keyFrame)
-        print(result)
-        differences.update({keyFrame:result[0]-exciting_label})
-
-
-    differences = sortDict(differences, True)
-    x = np.asarray(list(differences.keys()))
-    y = np.asarray(list(differences.values()))*-1
-
-    plot(x, y, ranges)
-
-    prime_frame = list(differences.keys())[0]
-    print("Prime frame", prime_frame)
-    print()
-    print(differences)
-
-    for r in ranges:
-        if r[0]==prime_frame:
-            lower_frame, upper_frame = r[1], r[2]
-            break
-
-    return prime_frame, lower_frame, upper_frame, frameSize, fps
-
 # does not work - we will use LIME instead
 def maskPixels(key_frame, lower_frame, upper_frame, frameSize, fps, box_size=100):
     print("Original video prediction:")
@@ -143,13 +63,3 @@ def maskPixels(key_frame, lower_frame, upper_frame, frameSize, fps, box_size=100
             #os.remove(video)
     print("\nFinished\n")
     print(sortDict(differences, False))
-
-
-if __name__ == "__main__":
-    print("Loading model...")
-    global model
-    model = keras.models.load_model('../../data/models/predict_model')
-    video_path = "../../data/videos/train_videos/62.mp4"
-    prime_frame, lower_frame, upper_frame, frameSize, fps = maskFrames(video_path, 10)
-    print('\n\n\n')
-    print(prime_frame, lower_frame, upper_frame, frameSize, fps)
