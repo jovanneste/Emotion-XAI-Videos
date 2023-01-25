0a18cfd97046df6a8470303f7aedb115dcf413a1
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-01-25 15:10:10 +0000

    use frame shapees rather than frame data

--- src/explainability/limeVideo.py
+++ src/explainability/limeVideo.py
@@ -32,56 +32,47 @@ class LimeVideoExplainer(object):
             kernel_fn, True, random_state=self.random_state)
 
     def explain_instances(self, video, classifier_fn):
+        # delete this
         def k(d):
             return np.sqrt(np.exp(-(d ** 2) / 0.25 ** 2))
         kf = partial(k)
 
         data, labels, order = self.data_labels(classifier_fn)
 
-        print("\nData and labels created")
-        print("Explain instance\n")
-
-        print("Data shape:", data.shape)
-        print("Labels shape:", labels.shape)
-
-        print()
         distances = []
         vidcap = cv2.VideoCapture(video)
         success, original_image = vidcap.read()
         for f in order:
             distances.append(self.distance(f, original_image[:,:,0]))
-        distances = np.asarray(distances)#.ravel()
+        distances = np.asarray(distances)
 
         ret_exp = VideoExplanation(video)
 
-        print("Distances shape:", distances.shape)
         top = np.argsort(labels)[-1:]
         ret_exp.top_labels = list(top)
         ret_exp.top_labels.reverse()
-        labels = labels.reshape(20, 2, 1)
-        print("Top", top)
+
         for label in top:
-            print("Calling explain_instance_with_data with:")
             print("data", data.shape)
             print("labels", labels.shape)
             print("distances", distances.shape)
             print("label", label.shape, label)
-            sys.exit()
+
             # imitating what happens in lime base when we call explain with instance
             weights = kf(distances)
-            labels_column = labels[:, label]
+            labels_column = np.squeeze(labels[:, label])
             used_features = self.base.feature_selection(data,
                                                labels_column,
                                                weights,
                                                100000,
-                                               method='auto')
-            print()
-            print(used_features)
-            print()
-            clf = SGDRegressor(alpha=0.01, fit_intercept=True,
+                                               method='none')
+
+            clf = Ridge(alpha=0.01, fit_intercept=True,
                         random_state=self.random_state)
-            print("Data shape:", data.shape)
-            print("Labels shape:", labels_column.shape)
+
+            print("\nCLF.fit called with")
+            print("Data[:,used_features]", data[:,used_features].shape)
+            print("labels_column", labels_column.shape)
 
             clf.fit(data[:,used_features], labels_column, sample_weight=weights)
             print("SGD model fitted")
@@ -107,14 +98,21 @@ class LimeVideoExplainer(object):
         # might have to sort this
         files = glob.glob('../../data/LIMEset/*')
         for f in files:
+            frames = []
             order.append(f)
-            print("\nFile:", f)
             d = load_sample(f)
-            data.append(d)
             label = classifier_fn(d)
-            print(label)
             labels.append(label)
-        return np.squeeze(np.array(data)), np.squeeze(np.array(labels)), order
+            capture = cv2.VideoCapture(f)
+            while(True):
+                success, frame = capture.read()
+                if success:
+                    frames.append(frame)
+                else:
+                    break
+            capture.release()
+            data.append(frames)
+        return np.array(data), np.squeeze(np.array(labels)), order
 
     def distance(self, video, original_image):
         vidcap = cv2.VideoCapture(video)
