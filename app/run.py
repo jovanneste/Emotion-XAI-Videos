876bf841d65ea7a8f4221f3e6bd705d9c1644153
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-02-02 11:37:21 +0000

    how to run added

--- app/run.py
+++ app/run.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# export FLASK_APP=hello.py
+# export FLASK_ENV=development
+# flask run
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
