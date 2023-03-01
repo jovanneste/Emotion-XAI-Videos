29b69d1468e7a2eaff386300c4231cd8e7a7b484
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-03-01 15:52:13 +0000

    app updates

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -15,7 +15,9 @@ function getOption(){
 function showFrames(){
   document.getElementById('frame1').style.display = "block";
   document.getElementById('frame2').style.display = "block";
-  document.getElementById('frame3').style.display = "block";
-  document.getElementById('frameoutput').style.display = "block";
+  // document.getElementById('frame3').style.display = "block";
+  // document.getElementById('frame4').style.display = "block";
+  document.getElementById('frameoutput1').style.display = "block";
+  // document.getElementById('frameoutput2').style.display = "block";
   document.getElementById('btnID').style.display = "none";
 };
