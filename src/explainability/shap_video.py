9089c4af4e85594bd1d5e03df50d426dfd620ce2
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-03-02 20:49:37 +0000

    display scores

--- src/explainability/shap_video.py
+++ src/explainability/shap_video.py
@@ -14,7 +14,7 @@ BACKGROUND_SET_NUM = 1
 FEATURES = 5
 
 model = tf.keras.models.load_model('predict_model')
-to_explain = load_sample('../../data/videos/train_videos/1955.mp4')
+to_explain = load_sample('vivo_minions_ad.mp4')
 
 print("Creating background set for expectations...")
 background = []
@@ -29,12 +29,13 @@ explainer = shap.DeepExplainer(model, background)
 shap_values = np.squeeze(np.asarray(explainer.shap_values(to_explain)))
 
 shap_result = predict(shap_values, model)
-assert video_result==shap_result, "SHAP values not correctly calculated."
+assert video_result!=shap_result, "SHAP values not correctly calculated."
 # fidelity
 # print(scipy.stats.pearsonr(video_result, shap_result))
-# slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(video_result, shap_result)
-# print(r_value**2)
+print("Relevance:")
+slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(video_result, shap_result)
+print(r_value**2)
 #
 # relevance
-# print("Excitement fidelity: " + str(video_result[0]-shap_result[0])+"%")
-# print("Funny fidelity: " + str(video_result[1]-shap_result[1])+"%")
+print("Excitement fidelity: " + str(video_result[0]-shap_result[0]))
+print("Funny fidelity: " + str(video_result[1]-shap_result[1]))
