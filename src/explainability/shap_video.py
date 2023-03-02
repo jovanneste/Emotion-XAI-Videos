2b90bf06c857fac3d7fe5ddccc349af21227337b
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-03-02 17:28:54 +0000

    background set created

--- src/explainability/shap_video.py
+++ src/explainability/shap_video.py
@@ -1,27 +1,29 @@
 import shap
 import tensorflow as tf
+# tf 2.0 is not compatible with SHAP deepExplain
 tf.compat.v1.disable_v2_behavior()
 import sys
 sys.path.append('../')
 from evaluateModel import *
-# import cv2
-#
+import numpy as np
+import glob
+import random
+
+BACKGROUND_SET_NUM = 1
+FEATURES = 1
+
 model = tf.keras.models.load_model('predict_model')
-# cvframe = cv2.imread('../../data/frames/frame16.jpg')[:,:,::-1]
-#
-data = load_sample('../../data/videos/train_videos/1940.mp4')
-#
-#
-# # explainer = shap.Explainer(model,max_evals=3841)
-# # explainer = shap.explainers.Permutation(model, max_evals = 3841)
-explainer = shap.DeepExplainer(model, data)
-print("Explainer:")
-print(explainer)
-# #
-#
-#
-#
-shap_values = explainer.shap_values(data)
-print(shap_values)
-print(len(shap_values))
-print(shap_values[0].shape)
+
+print("Creating background set for expectations...")
+background = []
+files = random.sample(glob.glob('../../data/videos/train_videos/*'),BACKGROUND_SET_NUM)
+for f in files:
+    background.append(load_sample(f))
+
+
+to_explain = load_sample('../../data/videos/train_videos/1955.mp4')
+video_result = predict(to_explain, model)
+explainer = shap.DeepExplainer(model, background)
+shap_values = np.squeeze(np.asarray(explainer.shap_values(to_explain)))
+
+shap_result = predict(shap_values, model)
